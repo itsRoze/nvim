7a4a95f25571e7358e3f3841110201958eae810a
elewis9989 <elewis9989@gmail.com>
2023-04-14 18:52:36 -0400

    add eslint, prettier, fix treesite bug

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = false,
+      follow_current_file = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = "",
+        expander_expanded = "",
+        expander_highlight = "NeoTreeExpander",
+      },
+    },
+  },
+}
